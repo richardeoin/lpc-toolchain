2c975137994d17dd46cad460c6417c2f88350b1c
Richard Eoin Meadows <richardeoin@gmail.com>
2013-05-27 22:44:50 +0100

    Modified chip files for LPC1769 variant

--- chip/startup_LPC17xx.c
+++ chip/startup_LPC17xx.c
@@ -29,12 +29,54 @@
 #define alias(f) __attribute__ ((alias(#f)))
 #define weak __attribute__ ((weak))
 
-/* Cortex-M# Core Interrupts */
+/* Cortex-M3 Core Interrupts */
 weak void Reset_Handler(void);
 weak void NMI_Handler(void) alias (Default_Handler);
 weak void HardFault_Handler(void);
+weak void MemManage_Handler(void) alias (Default_Handler);
+weak void BusFault_Handler(void) alias (Default_Handler);
+weak void UsageFault_Handler(void) alias (Default_Handler);
+weak void SVC_Handler(void) alias (Default_Handler);
+weak void DebugMon_Handler(void) alias (Default_Handler);
+weak void PendSV_Handler(void) alias (Default_Handler);
+weak void SysTick_Handler(void) alias (Default_Handler);
 
-/* LPCxxxx Chip Interrupts */
+/* LPC17xx Chip Interrupts */
+weak void WDT_IRQHandler(void) alias (Default_Handler);
+weak void TIMER0_IRQHandler(void) alias (Default_Handler);
+weak void TIMER1_IRQHandler(void) alias (Default_Handler);
+weak void TIMER2_IRQHandler(void) alias (Default_Handler);
+weak void TIMER3_IRQHandler(void) alias (Default_Handler);
+weak void UART0_IRQHandler(void) alias (Default_Handler);
+weak void UART1_IRQHandler(void) alias (Default_Handler);
+weak void UART2_IRQHandler(void) alias (Default_Handler);
+weak void UART3_IRQHandler(void) alias (Default_Handler);
+weak void PWM1_IRQHandler(void) alias (Default_Handler);
+weak void I2C0_IRQHandler(void) alias (Default_Handler);
+weak void I2C1_IRQHandler(void) alias (Default_Handler);
+weak void I2C2_IRQHandler(void) alias (Default_Handler);
+weak void SPI_IRQHandler(void) alias (Default_Handler);
+weak void SSP0_IRQHandler(void) alias (Default_Handler);
+weak void SSP1_IRQHandler(void) alias (Default_Handler);
+weak void PLL0_IRQHandler(void) alias (Default_Handler);
+weak void RTC_IRQHandler(void) alias (Default_Handler);
+weak void EINT0_IRQHandler(void) alias (Default_Handler);
+weak void EINT1_IRQHandler(void) alias (Default_Handler);
+weak void EINT2_IRQHandler(void) alias (Default_Handler);  
+weak void EINT3_IRQHandler(void) alias (Default_Handler);
+weak void ADC_IRQHandler(void) alias (Default_Handler);
+weak void BOD_IRQHandler(void) alias (Default_Handler);
+weak void USB_IRQHandler(void) alias (Default_Handler);
+weak void CAN_IRQHandler(void) alias (Default_Handler);
+weak void DMA_IRQHandler(void) alias (Default_Handler);
+weak void I2S_IRQHandler(void) alias (Default_Handler);
+weak void ENET_IRQHandler(void) alias (Default_Handler);
+weak void RIT_IRQHandler(void) alias (Default_Handler);
+weak void MCPWM_IRQHandler(void) alias (Default_Handler);
+weak void QEI_IRQHandler(void) alias (Default_Handler);
+weak void PLL1_IRQHandler(void) alias (Default_Handler);
+weak void USBActivity_IRQHandler(void) alias (Default_Handler);
+weak void CANActivity_IRQHandler(void) alias (Default_Handler);
 
 /* This is defined in the linker script */
 extern void __StackLimit(void);
@@ -50,6 +92,55 @@ const void *isr_vectors[] = {
   Reset_Handler,	  // The Reset handler
   NMI_Handler,		  // The NMI handler
   HardFault_Handler,	  // The Hard Fault Handler
+  MemManage_Handler,	  // The MPU Fault Handler
+  BusFault_Handler,	  // The Bus Fault Handler
+  UsageFault_Handler,     // The Usage Fault Handler
+  0,			  // Reserved
+  0,			  // Reserved
+  0,			  // Reserved
+  0,			  // Reserved
+  SVC_Handler,		  // SVCall Handler
+  DebugMon_Handler,	  // Debug Monitor Handler
+  0,			  // Reserved
+  PendSV_Handler,	  // The PendSV Handler
+  SysTick_Handler,	  // The SysTick Handler
+
+  /* LPC17xx Chip Interrupts */
+  WDT_IRQHandler,	  // WDT
+  TIMER0_IRQHandler,	  // Timer 0
+  TIMER1_IRQHandler,	  // Timer 1
+  TIMER2_IRQHandler,	  // Timer 2
+  TIMER3_IRQHandler,	  // Timer 3
+  UART0_IRQHandler,	  // UART0
+  UART1_IRQHandler,	  // UART1
+  UART2_IRQHandler,	  // UART2
+  UART3_IRQHandler,	  // UART3
+  PWM1_IRQHandler,	  // PWM1
+  I2C0_IRQHandler,	  // I2C0
+  I2C1_IRQHandler,	  // I2C1
+  I2C2_IRQHandler,	  // I2C2
+  SPI_IRQHandler,	  // SPI
+  SSP0_IRQHandler,	  // SSP0
+  SSP1_IRQHandler,	  // SSP1
+  PLL0_IRQHandler,	  // PLL0 (Main PLL)
+  RTC_IRQHandler,	  // RTC
+  EINT0_IRQHandler,	  // External Interrupt
+  EINT1_IRQHandler,	  // External Interrupt
+  EINT2_IRQHandler,	  // External Interrupt
+  EINT3_IRQHandler,	  // External Interrupt
+  ADC_IRQHandler,	  // ADC
+  BOD_IRQHandler,	  // BOD
+  USB_IRQHandler,	  // USB
+  CAN_IRQHandler,	  // CAN
+  DMA_IRQHandler,	  // GPDMA
+  I2S_IRQHandler,	  // I2S
+  ENET_IRQHandler,	  // Ethernet
+  RIT_IRQHandler,	  // Repetitive Interrupt Timer
+  MCPWM_IRQHandler,	  // Motor Control PWM
+  QEI_IRQHandler,	  // Quadrature Encoder
+  PLL1_IRQHandler,	  // PLL1 (USB PLL)
+  USBActivity_IRQHandler, // USB Activity Interrupt
+  CANActivity_IRQHandler, // CAN Activity Interrupt
 };
 
 /* These are defined in the linker script */
--- chip/system_LPC17xx.c
+++ chip/system_LPC17xx.c
@@ -1,7 +1,7 @@
 /**************************************************************************//**
- * @file     system_LPCxxxx.c
- * @brief    CMSIS Cortex-M# Device Peripheral Access Layer Source File for
- *           Device LPCxxxx
+ * @file     system_LPC17xx.c
+ * @brief    CMSIS Cortex-M3 Device Peripheral Access Layer Source File for
+ *           Device LPC17xx
  * @version  V3.10
  * @date     23. November 2012
  *
@@ -9,7 +9,7 @@
  *
  ******************************************************************************/
 /* Copyright (c) 2012 ARM LIMITED
-   Copyright (c) 2013 Richard Meadows - Added code for LPCxxxx
+   Copyright (c) 2013 Richard Meadows - Added code for LPC17xx
 
    All rights reserved.
    Redistribution and use in source and binary forms, with or without
@@ -38,8 +38,7 @@
 
 
 #include <stdint.h>
-#include "LPCxxxx.h"
-
+#include "LPC17xx.h"
 
 /*----------------------------------------------------------------------------
   DEFINES
@@ -48,35 +47,75 @@
 /*----------------------------------------------------------------------------
   Define clocks
  *----------------------------------------------------------------------------*/
-/* ToDo: add here your necessary defines for device initialization
-         following is an example for different system frequencies             */
-#define __HSI             ( 6000000UL)
 #define __XTAL            (12000000UL)    /* Oscillator frequency             */
-#define __SYS_OSC_CLK     (    ___HSI)    /* Main oscillator frequency        */
-
-#define __SYSTEM_CLOCK    (4*__XTAL)
-
+#define __SYS_OSC_CLK     (    __XTAL)    /* Main oscillator frequency        */
+#define __IRC             ( 4000000UL)    /* Internal RC Oscillator           */
+#define __RTC             (   32768UL)    /* Real Time Clock Oscillator       */
 
 /*----------------------------------------------------------------------------
   Clock Variable definitions
  *----------------------------------------------------------------------------*/
-/* ToDo: initialize SystemCoreClock with the system core clock frequency value
-         achieved after system intitialization.
-         This means system core clock frequency after call to SystemInit()    */
-uint32_t SystemCoreClock = __SYSTEM_CLOCK;  /*!< System Clock Frequency (Core Clock)*/
-
+uint32_t SystemCoreClock = __XTAL;    /*!< System Clock Frequency (Core Clock)*/
 
 /*----------------------------------------------------------------------------
   Clock functions
  *----------------------------------------------------------------------------*/
+int PLL0InputClock(void) {
+  /* Switch based on the System Clock Select */
+  /* See User Manual §4.4 Clock source selection multiplexer */
+  switch (LPC_SC->CLKSRCSEL & 0x3) {
+    case 0x0: return __IRC;
+    case 0x1: return __SYS_OSC_CLK;
+    case 0x2: return __RTC;
+    default: return 0;
+  }
+}
 void SystemCoreClockUpdate (void)            /* Get Core Clock Frequency      */
 {
-/* ToDo: add code to calculate the system frequency based upon the current
-         register settings.
-         This function can be used to retrieve the system core clock frequeny
-         after user changed register sittings.                                */
-  SystemCoreClock = __SYSTEM_CLOCK;
+  SystemCoreClock = PLL0InputClock();
+
+  /* If PLL0 is connected and enabled, account for the PLL */
+  /* See User Manual §4.5.5 PLL0 Status register */
+  if ((LPC_SC->PLL0STAT & 0x03000000) == 0x03000000) {
+    uint16_t m = (LPC_SC->PLL0STAT & 0x00007FFF) + 1;
+    uint8_t n = ((LPC_SC->PLL0STAT & 0x00FF0000) >> 16) + 1;
+
+    SystemCoreClock *= 2 * m;
+    SystemCoreClock /= n;
+  }
+
+  /* Divide by the CPU Clock Divider */
+  /* See User Manual §4.7.1 CPU Clock Configuration */
+  SystemCoreClock /= ((LPC_SC->CCLKCFG & 0xFF) + 1);
 }
+
+/* Enables (but doesn't switch to) PLL0 */
+void EnablePLL0(uint16_t m, uint8_t n) {
+  __disable_irq();
+
+  LPC_SC->PLL0CFG = ((n-1) << 16) | (m-1);
+  LPC_SC->PLL0FEED = 0xAA;
+  LPC_SC->PLL0FEED = 0x55;
+
+  LPC_SC->PLL0CON = 0x01;
+  LPC_SC->PLL0FEED = 0xAA;
+  LPC_SC->PLL0FEED = 0x55;
+
+  __enable_irq();
+}
+/* Switches to PLL0 */
+void SwitchToPLL0(void) {
+  /* Wait for the PLL to lock */
+  while(!(LPC_SC->PLL0STAT & 0x04000000));
+
+  __disable_irq();
+
+  LPC_SC->PLL0CON = 0x03;
+  LPC_SC->PLL0FEED = 0xAA;
+  LPC_SC->PLL0FEED = 0x55;
+
+  __enable_irq();
+}  
 
 /**
  * Initialize the system
@@ -89,8 +128,31 @@ void SystemCoreClockUpdate (void)            /* Get Core Clock Frequency      */
  */
 void SystemInit (void)
 {
-/* ToDo: add code to initialize the system
-         do not use global variables because this function is called before
-         reaching pre-main. RW section maybe overwritten afterwards.          */
-  SystemCoreClock = __SYSTEM_CLOCK;
+#ifdef USE_EXTERNAL_XTAL
+  /* Enable the Main Oscillator */
+  LPC_SC->SCS |= 0x0020;
+
+  /* Wait for it to stabilise */
+  while ((LPC_SC->SCS & 0x0040) == 0);
+#endif
+
+  /* Divide pllclk by 4 to get the CPU clock */
+  LPC_SC->CCLKCFG = 0x03;
+
+#ifdef USE_EXTERNAL_XTAL
+  /* Set the Clock Source for PLL0 to the Main Oscillator */
+  LPC_SC->CLKSRCSEL = 0x01;
+#endif
+
+  /* Set up PLL0 to multiply by 33.333.. */
+  EnablePLL0(100, 6);
+
+  /* And switch to PLL0 */
+  SwitchToPLL0();
+
+  /*
+   * The core is now running at 8.333.. times the XTAL frequency.
+   * If the crystal is 12MHz then this frequency is 100MHz.
+   * If the IRC is still being used this is 33.333...MHz.
+   */
 }
